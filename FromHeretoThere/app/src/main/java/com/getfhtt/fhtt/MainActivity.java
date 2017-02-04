c82b9dcbe9af82f4c86993178620e543334a8883
PfeifferH <15hp19@queensu.ca>
2017-02-04 15:45:15 -0500

    Added userStats method for calculating emissions saved, cost saved, calories burned

--- FromHeretoThere/app/src/main/java/com/getfhtt/fhtt/MainActivity.java
+++ FromHeretoThere/app/src/main/java/com/getfhtt/fhtt/MainActivity.java
@@ -13,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Journey Details");
     }
 
+    public void userStats(String distance) {
+        int metres = Integer.parseInt(distance);
 
+        //Constants for Calculations
+        float costPerKm = 0.106;
+        int emissionsPerKm = 255;
+        int caloriesPerKm = 112;
+
+        //Calculation for money saved
+        float moneySaved = costPerKm * metres;
+
+        //Calculation for emissions saved
+        int emissionsSaved = emissionsPerKm * metres;
+
+        //Calculation for caloriesBurned
+        int caloriesSaved = caloriesPerKm * metres;
+
+    }
 }
