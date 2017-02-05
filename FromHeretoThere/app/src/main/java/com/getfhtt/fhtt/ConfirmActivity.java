8eb498471fd5cc8f8543c4ed3613f4dcb59a4e5e
Ian <ianw3214@gmail.com>
2017-02-05 04:12:28 -0500

    Set up confirmation display structure

--- FromHeretoThere/app/src/main/java/com/getfhtt/fhtt/ConfirmActivity.java
+++ FromHeretoThere/app/src/main/java/com/getfhtt/fhtt/ConfirmActivity.java
@@ -9,5 +9,31 @@ public class ConfirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+
+        // get the data from the previous state to display on screen
+        String mode = savedInstanceState.getString("mode");
+        String emissons = savedInstanceState.getString("carEmissions");
+        int calories = savedInstanceState.getInt("calories");
+        String cost = savedInstanceState.getString("carCost");
+        String distance = savedInstanceState.getString("distance");
+        String duraction = savedInstanceState.getString("duration");
+        String url = savedInstanceState.getString("url");
+
+        // display data differently depending on the users choice of travel
+        switch(mode){
+            case "walking":{
+
+            } break;
+            case "bicycling": {
+
+            } break;
+            case "transit": {
+
+            } break;
+            case "driving":
+            default: {
+
+            } break;
+        }
     }
 }
