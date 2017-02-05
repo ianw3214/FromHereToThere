30acdfa174ed0ce3dcacf0c1ed6b011bb748cf22
Angelo Lu <luangelo.87@gmail.com>
2017-02-05 04:24:19 -0500

    Made basic

--- FromHeretoThere/app/src/main/java/com/getfhtt/fhtt/ConfirmActivity.java
+++ FromHeretoThere/app/src/main/java/com/getfhtt/fhtt/ConfirmActivity.java
@@ -1,7 +1,12 @@
 package com.getfhtt.fhtt;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ConfirmActivity extends AppCompatActivity {
 
@@ -9,15 +14,26 @@ public class ConfirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
-
+        Bundle extras = getIntent().getExtras();
         // get the data from the previous state to display on screen
-        String mode = savedInstanceState.getString("mode");
-        String emissons = savedInstanceState.getString("carEmissions");
-        int calories = savedInstanceState.getInt("calories");
-        String cost = savedInstanceState.getString("carCost");
-        String distance = savedInstanceState.getString("distance");
-        String duraction = savedInstanceState.getString("duration");
-        String url = savedInstanceState.getString("url");
+        String mode = extras.getString("mode");
+        String emissons = extras.getString("carEmissions");
+        int calories = extras.getInt("calories");
+        String cost = extras.getString("carCost");
+        String distance = extras.getString("distance");
+        String duraction = extras.getString("duration");
+        final String url = extras.getString("url");
+
+        TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
+        TextView tvText = (TextView) findViewById(R.id.tvText);
+        Button bContinue = (Button) findViewById(R.id.bContinue);
+        bContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
 
         // display data differently depending on the users choice of travel
         switch(mode){
@@ -35,5 +51,6 @@ public class ConfirmActivity extends AppCompatActivity {
 
             } break;
         }
+
     }
 }
